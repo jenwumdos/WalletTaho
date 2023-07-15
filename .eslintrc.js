e89b125fd7e043a8a74633eecc7919cd84accc5b
jenwumdos <139070746+jenwumdos@users.noreply.github.com>
2023-07-15 16:56:47 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
 // This is a non-ESM JS file, so this rule can't be followed.
 /* eslint-disable @typescript-eslint/no-var-requires */
 const {
+ rules: {
+    "@typescript-eslint/naming-convention":
