04fd710ef3899fb72eeb5053cb398c19436a83fc
jenwumdos <139070746+jenwumdos@users.noreply.github.com>
2023-12-04 11:48:35 +0200

    Update env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -8,3 +8,6 @@ declare module "webext-redux/lib/strategies/deepDiff/diff" {
 
 declare module "webext-redux/lib/strategies/deepDiff/patch" {
   // This should be : DiffStrategy, but importing webext-redux to reuse
+  // DiffStrategy results in an error augmenting the patch module.
+  export default function (): (oldObj: unknown, newObj: unknown) => unknown
+}
