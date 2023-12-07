c78d61347fc460e19d55b284188966441605d012
jenwumdos <139070746+jenwumdos@users.noreply.github.com>
2023-12-07 14:43:15 +0200

    Update env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -11,3 +11,5 @@ declare module "webext-redux/lib/strategies/deepDiff/patch" {
   // DiffStrategy results in an error augmenting the patch module.
   export default function (): (oldObj: unknown, newObj: unknown) => unknown
 }
+
+// Although you would expect this file to be unnecessary, removing it will
