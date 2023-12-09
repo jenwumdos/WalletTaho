bb016476a66ac037c8bbce03b517d75148a931b1
jenwumdos <139070746+jenwumdos@users.noreply.github.com>
2023-12-09 10:34:22 +0200

    Update env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -14,3 +14,5 @@ declare module "webext-redux/lib/strategies/deepDiff/patch" {
 
 // Although you would expect this file to be unnecessary, removing it will
 // result in a handful of type errors. See PR #196.
+
+declare module "styled-jsx/style"
